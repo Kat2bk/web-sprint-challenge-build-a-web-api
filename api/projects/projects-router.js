e1aa64c7c0b6ae7b064766026c426ebb1496d92e
unknown <Katherine2bk@gmail.com>
2021-06-13 22:01:28 -0700

    finished delete and get project actions request in projects-router.js

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -34,16 +34,42 @@ router.post('/', validateProject, async (req, res, next) => {
         next(error)
     }
 })
-// - [ ] `[POST] /api/projects`
 // - Returns the newly created project as the body of the response.
 // - If the request body is missing any of the required fields it responds with a status code 400.
-// - [ ] `[PUT] /api/projects/:id`
+
+router.put('/:id', validateProjectId, validateProject, async (req, res, next) => {
+    try {
+        const updated = await Projects.update(req.params.id, req.body)
+        res.status(200).json(updated)
+    } catch (error) {
+        next(error)
+    }
+})
+
 // - Returns the updated project as the body of the response.
 // - If there is no project with the given `id` it responds with a status code 404.
 // - If the request body is missing any of the required fields it responds with a status code 400.
-// - [ ] `[DELETE] /api/projects/:id`
+
+router.delete('/:id', validateProjectId, async (req, res, next) => {
+    try {
+       const deleted = await Projects.remove(req.params.id)
+       res.status(204).json(deleted)
+    } catch (error) {
+        next(error)
+    }
+})
+
 // - Returns no response body.
 // - If there is no project with the given `id` it responds with a status code 404.
+
+router.get('/:id/actions', validateProjectId, async (req, res, next) => {
+    try {
+        const actions = await Projects.getProjectActions(req.params.id)
+        res.status(200).json(actions)
+    } catch (error) {
+        next(error)
+    }
+})
 // - [ ] `[GET] /api/projects/:id/actions`
 // - Returns an array of actions (could be empty) belonging to a project with the given `id`.
 // - If there is no project with the given `id` it responds with a status code 404.
