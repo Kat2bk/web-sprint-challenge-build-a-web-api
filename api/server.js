5c7eb7c4d498520f22d6a0c927dc225a67e0e7bb
unknown <Katherine2bk@gmail.com>
2021-06-13 20:53:36 -0700

    set up basic structure for projects-router, exported router into server.js

--- api/server.js
+++ api/server.js
@@ -2,7 +2,10 @@ require('dotenv').config()
 
 const express = require('express');
 const server = express();
+const projectsRouter = require('../api/projects/projects-router');
 server.use(express.json());
+
+server.use('/api/projects', projectsRouter)
 
 server.get('/', (req, res) => {
     res.json({message: 'Welcome to the API'})
