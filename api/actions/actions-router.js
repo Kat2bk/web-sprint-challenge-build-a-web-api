2b566e23cfac181bd7143495b82f7b824fc03d2b
unknown <Katherine2bk@gmail.com>
2021-06-13 22:40:47 -0700

    strange error at position 72 in actions-router ... put request wants to work, tried several ways of formating json differently, but doesn't go through. delete/remove request works fine.

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -36,13 +36,66 @@ router.get('/:id', (req, res, next) => {
 //   - Returns the newly created action as the body of the response.
 //   - If the request body is missing any of the required fields it responds with a status code 400.
 //   - When adding an action make sure the `project_id` provided belongs to an existing `project`.
+
+router.post('/:id', (req, res, next) => {
+    Actions.insert({    
+        project_id: req.params.id,
+        description: req.body.description,
+        notes: req.body.notes,
+        completed: req.body.completed
+    })
+    .then(newPost => {
+        if (!req.params.id) {
+           return res.status(404).json({message: "project not found"})
+        } else if (!req.body.description || !req.body.notes) {
+            return res.status(400).json({message: "please fill out all required fields"})
+        } else {
+          return  res.status(201).json(newPost)
+        }
+    })
+    .catch(error => {
+        next(error)
+    })
+})
+
 // - [ ] `[PUT] /api/actions/:id`
 //   - Returns the updated action as the body of the response.
 //   - If there is no action with the given `id` it responds with a status code 404.
 //   - If the request body is missing any of the required fields it responds with a status code 400.
+
+router.put('/:id', (req, res, next) => {
+    Actions.update(req.params.id, req.body)
+    .then(action => {
+        if (!req.params.id) {
+            res.status(404).json({message: "action not found"})
+        } else if (!req.body.description || !req.body.notes) {
+            res.status(400).json({message: "please fill out all required fields"})
+        } else {
+            res.status(200).json(action)
+        }
+    })
+    .catch(error => {
+        next(error)
+    })
+})
+
 // - [ ] `[DELETE] /api/actions/:id`
 //   - Returns no response body.
 //   - If there is no action with the given `id` it responds with a status code 404.
+
+router.delete('/:id', (req, res, next) => {
+    Actions.remove(req.params.id)
+    .then(action => {
+        if (!action) {
+            res.status(404).json({message: "action not found"})
+        } else {
+            res.status(204).json(action)
+        }
+    })
+    .catch(error => {
+        next(error)
+    })
+})
 
 router.use((err, req, res, next) => {
     res.status(500).json({
