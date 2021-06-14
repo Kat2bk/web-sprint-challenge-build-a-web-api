3395df87495419281c07e704a99b1401f8bd8fca
unknown <Katherine2bk@gmail.com>
2021-06-13 22:03:28 -0700

    plugged in actionsRouter in server.js

--- api/server.js
+++ api/server.js
@@ -3,9 +3,11 @@ require('dotenv').config()
 const express = require('express');
 const server = express();
 const projectsRouter = require('../api/projects/projects-router');
+const actionsRouter = require('../api/actions/actions-router');
 server.use(express.json());
 
 server.use('/api/projects', projectsRouter)
+server.use('/api/actions', actionsRouter)
 
 server.get('/', (req, res) => {
     res.json({message: 'Welcome to the API'})
